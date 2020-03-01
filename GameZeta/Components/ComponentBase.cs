c53f9b6b4373ee29b73d3091d94dbdd3b06fa227
PavelBfl <33580861+PavelBfl@users.noreply.github.com>
2020-03-01 21:04:22 +0500

    + Реализован базовый проект

---
+++ GameZeta/Components/ComponentBase.cs
@@ -0,0 +1,23 @@
+﻿using RevertedModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GameZeta.Components
+{
+	public class ComponentBase : Component, ITrackObject
+	{
+		public ComponentBase(TrackDispatcher trackDispatcher)
+		{
+			TrackDispatcher = trackDispatcher ?? throw new ArgumentNullException();
+		}
+		public ComponentBase()
+			: this(TrackDispatcher.Default)
+		{
+
+		}
+
+		public TrackDispatcher TrackDispatcher { get; } = null;
+	}
+}
---
+++ GameZeta/Components/Location.cs
@@ -0,0 +1,26 @@
+﻿using RevertedModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+
+namespace GameZeta.Components
+{
+	public class Location : ComponentBase
+	{
+		public Location()
+			: base()
+		{
+			
+		}
+		public Location(TrackDispatcher trackDispatcher)
+			: base(trackDispatcher)
+		{
+
+		}
+
+		public PointF Position { get => position.Value; set => position.Value = value; }
+		private ITrackValue<PointF> position = null;
+	}
+}
